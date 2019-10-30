515822e03dbb5004148be7600259a34c2277b4d8
cye <cye@vivans.net>
2019-10-30 18:41:53 +0900

    cardList 대문자 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
-import { Query } from "../graphql-types"
+import React from 'react';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import { Query } from '../graphql-types';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import CardList from "../components/cardList"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+import CardList from '../components/CardList';
 
 const IndexPage: React.FC = () => {
-  return (
-    <Layout>
-      <CardList />
-    </Layout>
-  )
-}
+    return (
+        <Layout>
+            <CardList />
+        </Layout>
+    );
+};
 
-export default IndexPage
+export default IndexPage;
